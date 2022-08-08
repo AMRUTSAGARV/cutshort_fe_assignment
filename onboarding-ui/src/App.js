34265be9f2de0320781d9c2b5e74201c4beb6b1e
Amrut Sagar <amrutsagar06@gmail.com>
2022-08-08 21:13:13 +0530

    getting started with tailwind

---
+++ onboarding-ui/src/App.js
@@ -0,0 +1,12 @@
+import "./input.css";
+
+function App() {
+  return (
+    <div className="flex h-screen m-1">
+      {/* <div className="border-4 p-10 w-1/6"></div>
+      <div className="border-4 p-10 w-full"></div> */}
+    </div>
+  );
+}
+
+export default App;
