08bded0788535fbc84d4d547468737219f076114
Amrut Sagar <amrutsagar06@gmail.com>
2022-08-09 20:53:47 +0530

    first page

--- onboarding-ui/src/App.js
+++ onboarding-ui/src/App.js
@@ -1,10 +1,14 @@
+import PageOne from "./components/pages/PageOne";
 import "./input.css";
 
 function App() {
   return (
-    <div className="flex h-screen m-1">
-      {/* <div className="border-4 p-10 w-1/6"></div>
-      <div className="border-4 p-10 w-full"></div> */}
+    // <div className="flex h-screen m-1">
+    //   <div className="border-4 p-10 w-1/6"></div>
+    //   <div className="border-4 p-10 w-full"></div>
+    // </div>
+    <div>
+      <PageOne />
     </div>
   );
 }
