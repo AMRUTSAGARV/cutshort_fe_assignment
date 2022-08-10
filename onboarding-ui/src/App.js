025c88cd268f48b99cfc208e9875dcf8f90308b5
Amrut Sagar <amrutsagar06@gmail.com>
2022-08-10 14:57:00 +0530

    milti step action

--- onboarding-ui/src/App.js
+++ onboarding-ui/src/App.js
@@ -1,14 +1,45 @@
-import PageOne from "./components/pages/PageOne";
+import { useState } from "react";
+
+import Stepper from "./components/Stepper";
+import StepperControl from "./components/StepperControl";
+import Account from "./components/steps/Account";
+import Details from "./components/steps/Details";
+import Final from "./components/steps/Final";
 import "./input.css";
 
 function App() {
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const steps = ["Account information", "Personal Details", "Complete"];
+
+  const displayStep = (step) => {
+    switch (step) {
+      case 1:
+        return <Account />;
+      case 2:
+        return <Details />;
+      case 3:
+        return <Final />;
+      default:
+    }
+  };
+  const handleClick = (direction) => {
+    let newStep = currentStep;
+    direction == "next" ? newStep++ : newStep--;
+    //check if steps are within bounds .
+    newStep > 0 && newStep < steps.length && setCurrentStep(newStep);
+  };
+
   return (
-    // <div className="flex h-screen m-1">
-    //   <div className="border-4 p-10 w-1/6"></div>
-    //   <div className="border-4 p-10 w-full"></div>
-    // </div>
-    <div>
-      <PageOne />
+    <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
+      <div className="container horizontal mt-5">
+        <Stepper steps={steps} currentStep={currentStep} />
+      </div>
+      <StepperControl
+        handleClick={handleClick}
+        currentStep={currentStep}
+        steps={steps}
+      />
     </div>
   );
 }
--- onboarding-ui/src/components/pages/PageFour.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PageFour = () => {
-  return <div>PageFour</div>;
-};
-
-export default PageFour;
