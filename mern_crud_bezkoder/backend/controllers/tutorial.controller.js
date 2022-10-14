a8d1ea1853e910812f3c31e908531a44bd3d3312
Amrut Sagar <amrutsagar06@gmail.com>
2022-10-14 21:11:16 +0530

    controllers

--- mern_crud_bezkoder/backend/controllers/tutorial.controller.js
+++ mern_crud_bezkoder/backend/controllers/tutorial.controller.js
@@ -2,25 +2,151 @@ const db = require("../models");
 const Tutorial = db.tutorials;
 
 // Create and Save a new Tutorial
-exports.create = (req, res) => {};
+exports.create = (req, res) => {
+  //validate request
+  if (!req.body.title) {
+    res.status(404).send({ message: "Content cannot be empty." });
+    return;
+  }
+  //create a tutorial.
+  const tutorial = new Tutorial({
+    title: req.body.title,
+    description: req.body.description,
+    published: req.body.published ? req.body.published : false,
+  });
+  //save totorial in database.
+  tutorial
+    .save(tutorial)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occured while creating the tutorial.",
+      });
+    });
+};
 
 // Retrieve all Tutorials from the database.
-exports.findAll = (req, res) => {};
+exports.findAll = (req, res) => {
+  const title = req.query.title;
+  var condition = title
+    ? { title: { $regex: new RegExp(title), $options: "i" } }
+    : {};
+
+  Tutorial.find(condition)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occured while retrieving tutorials.",
+      });
+    });
+};
 
 // Find a single Tutorial with an id
-exports.findOne = (req, res) => {};
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Tutorial.findById(id)
+    .then((data) => {
+      if (!data)
+        res.status(404).send({ message: "Not found Tutorial with id " + id });
+      else res.send(data);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Tutorial with id=" + id });
+    });
+};
 
 // Update a Tutorial by the id in the request
-exports.update = (req, res) => {};
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!",
+    });
+  }
+
+  const id = req.params.id;
+
+  Tutorial.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found!`,
+        });
+      } else res.send({ message: "Tutorial was updated successfully." });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating Tutorial with id=" + id,
+      });
+    });
+};
 
 //Delete a Tutorial with the specified id in the request
-exports.delete = (req, res) => {};
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Tutorial.findByIdAndRemove(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete tutorial with id=${id}. Maybe tutorial was not found.`,
+        });
+      } else {
+        res.send({
+          message: "Tutorial was deleted succesfully.",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete tutorial with id=" + id,
+      });
+    });
+};
 
 // Delete all Tutorials from the database.
-exports.deleteAll = (req, res) => {};
+exports.deleteAll = (req, res) => {
+  Tutorial.deleteMany({})
+    .then((data) => {
+      res.send({
+        message: `${data.deletedCount} Tutorials were deleted successfully!`,
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all tutorials.",
+      });
+    });
+};
 
 //Published are a button in frontend to publish (status:published/pending)
 //in models published there is Boolean
 //which means in database it will be true or false.
 // Find all published Tutorials
-exports.findAllPublished = (req, res) => {};
+exports.findAllPublished = (req, res) => {
+  Tutorial.find({ published: true })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials.",
+      });
+    });
+};
+
+// The req object represents the HTTP request and has properties
+// for the request query string, parameters, body, and HTTP headers.
+// The res object represents the HTTP response that an Express app sends
+// when it gets an HTTP request. In our case, we are sending a text
+// Hello World whenever a request is made to the route / .
