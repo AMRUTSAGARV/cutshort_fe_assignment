51be91ff91192cd7c63319a942e00e202a9f0cc9
Amrut Sagar <amrutsagar06@gmail.com>
2022-10-15 11:25:14 +0530

    services

--- mern_crud_bezkoder/frontend/src/http-common.js
+++ mern_crud_bezkoder/frontend/src/http-common.js
@@ -1,1 +1,9 @@
 // http-common.js initializes axios with HTTP base Url and headers.
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://localhost:8080/api",
+  headers: {
+    "Content-type": "application/json",
+  },
+});
--- mern_crud_bezkoder/frontend/src/services/tutorial.service.js
+++ mern_crud_bezkoder/frontend/src/services/tutorial.service.js
@@ -1,1 +1,36 @@
 // TutorialDataService has methods for sending HTTP requests to the Apis.
+// In this step, we’re gonna create a service that uses axios object above to send HTTP requests.
+// We call axios get, post, put, delete method corresponding to HTTP Requests: GET, POST, PUT, DELETE to make CRUD Operations.
+import http from "../http-common";
+
+class TutorialDataService {
+  getAll() {
+    return http.get("/tutorials");
+  }
+
+  get(id) {
+    return http.get(`/tutorials/${id}`);
+  }
+
+  create(data) {
+    return http.post("/tutorials", data);
+  }
+
+  update(id, data) {
+    return http.put(`/tutorials/${id}`, data);
+  }
+
+  delete(id) {
+    return http.delete(`/tutorials/${id}`);
+  }
+
+  deleteAll() {
+    return http.delete(`/tutorials`);
+  }
+
+  findByTitle(title) {
+    return http.get(`/tutorials?title=${title}`);
+  }
+}
+
+export default new TutorialDataService();
