c99772310e10a6b39f95b8e61ca7e1ca5bbacd74
Amrut Sagar <amrutsagar06@gmail.com>
2022-10-16 14:15:58 +0530

    edit tutorial

--- mern_crud_bezkoder/frontend/src/components/tutorial.js
+++ mern_crud_bezkoder/frontend/src/components/tutorial.js
@@ -1,7 +1,155 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import TutorialService from "../services/TutorialService";
 
 const Tutorial = () => {
-  return <div>Tutorial</div>;
+  const { id } = useParams();
+  let navigate = useNavigate();
+
+  const initialTutorialState = {
+    id: null,
+    title: "",
+    description: "",
+    published: false,
+  };
+
+  const [currentTutorial, setCurrentTutorial] = useState(initialTutorialState);
+  const [message, setMessage] = useState("");
+
+  const getTutorial = (id) => {
+    TutorialService.get(id)
+      .then((res) => {
+        setCurrentTutorial(res.data);
+        console.log(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    if (id) getTutorial(id);
+  }, [id]); // [id] -> the dependency that should trigger the callback function to run.
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setCurrentTutorial({ ...currentTutorial, [name]: value });
+  };
+
+  const updatePublished = (status) => {
+    var data = {
+      id: currentTutorial.id,
+      title: currentTutorial.title,
+      description: currentTutorial.description,
+      published: status,
+    };
+
+    TutorialService.update(currentTutorial.id, data)
+      .then((res) => {
+        setCurrentTutorial({ ...currentTutorial, published: status });
+        console.log(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const updateTutorial = () => {
+    TutorialService.update(currentTutorial.id, currentTutorial)
+      .then((res) => {
+        console.log(res.data);
+        setMessage("The tutorial was updated successfully!");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const deleteTutorial = () => {
+    TutorialService.remove(currentTutorial.id)
+      .then((res) => {
+        console.log(res.data);
+        navigate("/tutorials");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <div>
+      {currentTutorial ? (
+        <div className="edit-form">
+          <h4>Tutorial</h4>
+          <form>
+            <div className="form-group">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                className="form-control"
+                id="title"
+                name="title"
+                value={currentTutorial.title}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <input
+                type="text"
+                className="form-control"
+                id="description"
+                name="description"
+                value={currentTutorial.description}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label>
+                <strong>Status:</strong>
+              </label>
+              {currentTutorial.published ? "Published" : "Pending"}
+            </div>
+          </form>
+
+          {currentTutorial.published ? (
+            <button
+              className="badge badge-primary mr-2"
+              onClick={() => updatePublished(false)}
+            >
+              UnPublish
+            </button>
+          ) : (
+            <button
+              className="badge badge-primary mr-2"
+              onClick={() => updatePublished(true)}
+            >
+              Publish
+            </button>
+          )}
+
+          <button className="badge badge-danger mr-2" onClick={deleteTutorial}>
+            Delete
+          </button>
+
+          <button
+            type="submit"
+            className="badge badge-success"
+            onClick={updateTutorial}
+          >
+            Update
+          </button>
+          <p>{message}</p>
+        </div>
+      ) : (
+        <div>
+          <br />
+          <p>Please click on a Tutorial...</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Tutorial;
